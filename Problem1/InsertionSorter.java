4056fdd7c0a58543ce1ab5b1d5a06ccf2fffcc3b
Henrique Kishida <henrique_ak47@hotmail.com>
2022-11-25 15:41:24 -0300

    Update InsertionSorter.java
    Deleted "changed" boolean and changed If state for while state.

--- Problem1/InsertionSorter.java
+++ Problem1/InsertionSorter.java
@@ -7,23 +7,18 @@ public class InsertionSorter implements Sorter {
         System.out.println("Sorting with Insertion Sort.");
 
         int[] sortedArray = array.clone();
-        boolean changed = true;
 
-        while (changed) {
-            changed = false;
 
             for (int i = 1; i < sortedArray.length; i++) {
                 int tmp = sortedArray[i];
                 int j = i - 1;
-
-                if (j >= 0 && sortedArray[j] > tmp) {
+                
+                while (j >= 0 && sortedArray[j] > tmp) {
                     sortedArray[j + 1] = sortedArray[j];
                     j--;
                     sortedArray[j + 1] = tmp;
-                    changed = true;
                 }
             }
-        }
 
         return sortedArray;
     }
