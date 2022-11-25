1cf95b5fc39890a998da5d32dd275e055be85814
Henrique Kishida <henrique_ak47@hotmail.com>
2022-11-25 15:09:00 -0300

    Update BubbleSorter.java

--- Problem1/BubbleSorter.java
+++ Problem1/BubbleSorter.java
@@ -6,19 +6,20 @@ public class BubbleSorter implements Sorter {
     public int[] Sort(int[] array) {
         System.out.println("Sorting array using Bubble Sort");
         int[] sortedArray = array.clone();
-        boolean moved;
+        boolean moved = true;
 
-        do {
+        while (moved) {
             moved = false;
-            for (int i = 0; i < (sortedArray.length - 1); i++) {
+            
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                int tmp = sortedArray[i];
                 if (sortedArray[i] > sortedArray[i + 1]) {
-                    int tmp = sortedArray[i];
                     sortedArray[i] = sortedArray[i + 1];
                     sortedArray[i + 1] = tmp;
                     moved = true;
                 }
             }
-        } while (moved);
+        }
 
         return sortedArray;
     }
